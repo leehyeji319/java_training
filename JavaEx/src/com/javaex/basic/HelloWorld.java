4a09a40eaf93c7e1ea2478e3ebeab10b35186aa4
leehyeji <zzambbang@gmail.com>
2020-08-04 11:04:50 +0900

    첫 번째 커밋

---
+++ JavaEx/src/com/javaex/basic/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.javaex.basic;
+
+
+//화면에 Hello,World라 출력 해주자
+
+public class HelloWorld {
+	//직접 실행을 위해서는 main메서드가 있어야한다.
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World!");
+		
+	}
+
+}
