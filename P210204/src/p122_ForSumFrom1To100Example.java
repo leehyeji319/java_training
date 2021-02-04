b872f9bece6209ab7373ae00e2d71efd05453498
modsiw <zzambbang@gmail.com>
2021-02-04 15:21:32 +0900

    for문까지

---
+++ P210204/src/p122_ForSumFrom1To100Example.java
@@ -0,0 +1,11 @@
+public class p122_ForSumFrom1To100Example {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for(int i=1; i<=100; i++){
+            sum += i;
+        }
+
+        System.out.println("1~100합 : " + sum);
+    }
+}
