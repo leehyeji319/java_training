96c63a768fafb47a8bc82d356b79bb50f94f58d3
modsiw <zzambbang@gmail.com>
2021-02-04 15:59:06 +0900

    while문까지

---
+++ P210204/src/p122_ForFloatCounterExample.java
@@ -0,0 +1,7 @@
+public class p122_ForFloatCounterExample {
+    public static void main(String[] args) {
+        for(float x=0.1f; x<=1.0f; x+=0.1f){
+            System.out.println(x);
+        }
+    }
+}
