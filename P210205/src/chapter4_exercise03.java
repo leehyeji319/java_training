23e54a972d045413e1b88812d58b5e898729f444
modsiw <zzambbang@gmail.com>
2021-02-05 14:41:08 +0900

    4장 연습문제

--- P210205/src/chapter4_exercise03.java
+++ P210205/src/chapter4_exercise03.java
@@ -2,6 +2,13 @@ public class chapter4_exercise03 {
     public static void main(String[] args) {
         //작성위치 -
         //for문을 이용하여 1부터 100까지의 정수 중에서 3의 배수의 총합을 구하는 코드를작성해보세요 
-        
+        int i; 
+        int sum = 0;
+        for(i=1; i<=100; i++){
+            if(i%3 == 0){
+                sum += i;
+            }
+        }
+        System.out.println(sum);
     }
 }
